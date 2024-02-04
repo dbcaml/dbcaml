812ad0164cc1583f7aaff298db97f53810387312
emilpriver <emil@priver.dev>
2024-02-05 00:01:10 +0100

    added generic for mapping to type

--- dbcaml/row.ml
+++ dbcaml/row.ml
@@ -1,7 +1,7 @@
 (* Row.t will be used to create the row type we want to return to the users*)
 type t = string option list
 
-let row_to_type (row : t) =
+let row_to_type (row : t) (t : 'to_type) =
   List.fold_left
     (fun acc s ->
       match s with
@@ -10,4 +10,4 @@ let row_to_type (row : t) =
     []
     row
 
-let rows_to_type (rows : t list) = List.map row_to_type rows
+let rows_to_type (rows : t list) (t : 'to_type) = List.map row_to_type rows
