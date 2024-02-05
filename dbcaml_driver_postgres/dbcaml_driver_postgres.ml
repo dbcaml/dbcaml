69e1e5e07d09b22327ffe73fdadc7abb7d520821
emilpriver <emil@priver.dev>
2024-02-05 23:50:59 +0100

    Worked on driver for postgres

--- dbcaml_driver_postgres/dbcaml_driver_postgres.ml
+++ dbcaml_driver_postgres/dbcaml_driver_postgres.ml
@@ -2,12 +2,23 @@ open Postgresql
 
 let ( let* ) = Result.bind
 
-let remove_first_slash s =
-  try
-    let index = String.index s '/' in
-    String.sub s 0 index ^ String.sub s (index + 1) (String.length s - index - 1)
-  with
-  | Not_found -> s (* Return the original string if no slash is found *)
+let wait_for_result c =
+  c#consume_input;
+  while c#is_busy do
+    ignore (Unix.select [Obj.magic c#socket] [] [] (-1.0));
+    c#consume_input
+  done
+
+let fetch_result c =
+  wait_for_result c;
+  c#get_result
+
+let fetch_single_result c =
+  match fetch_result c with
+  | None -> assert false
+  | Some r ->
+    assert (fetch_result c = None);
+    r
 
 module Postgres = struct
   type config = { conninfo: string }
@@ -19,15 +30,31 @@ module Postgres = struct
      * Create the execute function that also use the PGOCaml.connection to send a request to Postgres database. 
      * This function is used by the Connection.make function to create a new connection
      *)
-    let execute (_ : 'conn) params query :
-        (* TODO: remove this type as it should be inherited*)
-        (string option list list, [> `msg of string ]) Io.io_result =
-      let result = c#exec ~expect:[Tuples_ok] ~params query in
-      let rows =
-        result#get_all_lst |> List.map (fun x -> List.map (fun r -> Some r) x)
-      in
+    let execute (conn : connection) (params : string array) (query : string) :
+        (Dbcaml.Row.t list, 'b) Io.io_result =
+      try
+        let stmt_name =
+          Digest.to_hex (Printf.sprintf "Dbcaml.%s" (Digest.to_hex query))
+        in
+        let param_types =
+          Array.make (Array.length params) (oid_of_ftype INT8)
+        in
+        ignore (conn#prepare ~param_types stmt_name query);
 
-      Ok rows
+        let result = conn#exec_prepared ~params stmt_name in
+
+        match result#status with
+        | Command_ok ->
+          let res = result#get_all_lst in
+
+          let rows = List.map (fun x -> List.map unescape_bytea x) res in
+          Ok rows
+        | _ -> Error `Io_error
+      with
+      | Postgresql.Error e -> Error e
+      | e ->
+        print_endline (Printexc.to_string e);
+        Error `Io_error
     in
 
     (* Create a new connection while we also want to use to create a PID *)
