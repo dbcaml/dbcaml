e307f4216f11691a7da28787b71de6476b24fcda
Emil Privér <emil@priver.dev>
2024-02-12 13:49:58 +0100

    Worked on acceptor

--- dbcaml/acceptor.ml
+++ dbcaml/acceptor.ml
@@ -4,17 +4,12 @@ open Logger.Make (struct
   let namespace = ["dbcaml"; "acceptor"]
 end)
 
-type ('ctx, 'err) state = {
-  driver: Driver.t;
+type ('ctx, 'config, 'err) state = {
   initial_ctx: 'ctx;
   handler: (module Driver.Intf with type config = 'config);
 }
 
-let rec accept_loop conn_sup state =
-  match Net.Tcp_listener.accept state.socket with
-  | Ok (conn, peer) -> handle_conn conn_sup state conn peer
-  | Error err ->
-    error (fun f -> f "Error accepting connection: %a" IO.pp_err err)
+let rec accept_loop conn_sup state = handle_conn conn_sup state conn peer
 
 and handle_conn conn_sup state conn peer =
   let accepted_at = Ptime_clock.now () in
@@ -45,7 +40,6 @@ let start_link state =
   in
   Ok pid
 
-let child_spec ~socket ?(buffer_size = 1_024 * 50) transport handler initial_ctx
-    =
-  let state = { socket; buffer_size; transport; handler; initial_ctx } in
+let child_spec handler initial_ctx =
+  let state = { handler; initial_ctx } in
   Supervisor.child_spec start_link state
