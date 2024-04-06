13594592b271da7bacf8f225f36c86f9d3c44f74
emilpriver <emil@priver.dev>
2024-04-06 23:01:45 +0200

    WIP

--- examples/basic_postgres/main.ml
+++ examples/basic_postgres/main.ml
@@ -1,5 +1,7 @@
 open Riot
 
+let ( let* ) = Result.bind
+
 open Logger.Make (struct
   let namespace = ["dbcaml"; "dbcaml_driver_postgres"]
 end)
@@ -12,52 +14,18 @@ let () =
 
   info (fun f -> f "Starting application");
 
-  let driver =
-    Dbcaml_driver_postgres.connection
-      "postgresql://postgres:postgres@localhost:6432/postgres?sslmode=disabled"
+  let pool_id =
+    match
+      Silo_postgres.start
+        ~connections:10
+        "postgresql://postgres:postgres@localhost:6432/postgres?sslmode=disabled"
+    with
+    | Ok conn -> conn
+    | Error e -> failwith e
   in
 
-  let _ = Dbcaml.start_link ~connections:10 driver |> Result.get_ok in
-  (*
-  (match Dbcaml.fetch_many pool_id "select * from users" with
+  (match Silo_postgres.fetch_one pool_id ~query:"select * from users" with
   | Ok x -> Printf.printf "%Sn" (String.of_bytes x)
   | Error x -> print_endline (Dbcaml.Res.execution_error_to_string x));
 
-  (match
-     Dbcaml.fetch_many
-       pool_id
-       ~params:[Dbcaml.Param.Number 20]
-       "select * from users where id > $1"
-   with
-  | Ok x -> Printf.printf "fetch_one response %Sn" (String.of_bytes x)
-  | Error x -> print_endline (Dbcaml.Res.execution_error_to_string x));
-*)
-  (*
-    (* Fetch multiple rows from the database *)
-    (match
-       Silo.fetch_many
-         pool_id
-         ~params:[Dbcaml.Connection.Number 1]
-         "select * from users where id = $1"
-     with
-    | Ok x ->
-      List.iter
-        (fun x ->
-          let rows = Silo.Row.row_to_type x in
-          (* Iterate over each column and print it's values *)
-          List.iter (fun x -> print_endline x) rows)
-        x
-    | Error x -> print_endline (Dbcaml.Res.execution_error_to_string x));
-
-    (* Exec a query to the database *)
-    (match
-       Silo.exec
-         pool_id
-         ~params:[Dbcaml.Connection.Number 1]
-         "select * from users where id = $1"
-     with
-    | Ok _ -> print_endline "Executed successfully"
-    | Error x -> print_endline (Dbcaml.Res.execution_error_to_string x));
-
-    () *)
   ()
---
+++ silo-postgres/lib/silo_postgres.ml
@@ -0,0 +1,9 @@
+let start ?(connections = 10) conninfo =
+  let driver = Dbcaml_driver_postgres.connection conninfo in
+
+  Dbcaml.start_link ~connections driver
+
+let fetch_one ?(params = []) connection_manager_pid ~query =
+  let result = Dbcaml.raw_execute connection_manager_pid params query in
+
+  result
